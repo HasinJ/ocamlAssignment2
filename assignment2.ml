5aea9f4b5c3e0d193aeee91eb887a4416f6c5e36
HasinJ <hasinchoudhuryj@gmail.com>
2021-03-01 22:37:05 -0500

    initial commit

---
+++ assignment2.ml
@@ -0,0 +1,88 @@
+open List
+
+let rec cond_dup l f =
+[]
+
+
+let rec n_times (f, n, v) =
+  0
+
+let rec zipwith f l1 l2 =
+  []
+
+let buckets p l =
+  []
+
+let fib_tailrec n =
+  0
+
+let assoc_list lst =
+  []
+
+let ap fs args =
+  []
+
+(********)
+(* Done *)
+(********)
+
+let _ = print_string ("Testing your code ...\n")
+
+let main () =
+  let error_count = ref 0 in
+
+  (* Testcases for cond_dup *)
+  let _ =
+    try
+      assert (cond_dup [3;4;5] (fun x -> x mod 2 = 1) = [3;3;4;5;5])
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for n_times *)
+  let _ =
+    try
+      assert (n_times((fun x-> x+1), 50, 0) = 50)
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for zipwith *)
+  let _ =
+    try
+      assert ([5;7] = (zipwith (+) [1;2;3] [4;5]))
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for buckets *)
+  let _ =
+    try
+      assert (buckets (=) [1;2;3;4] = [[1];[2];[3];[4]]);
+      assert (buckets (=) [1;2;3;4;2;3;4;3;4] = [[1];[2;2];[3;3;3];[4;4;4]]);
+      assert (buckets (fun x y -> (=) (x mod 3) (y mod 3)) [1;2;3;4;5;6] = [[1;4];[2;5];[3;6]])
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for fib_tailrec *)
+  let _ =
+    try
+      assert (fib_tailrec 50 = 12586269025)
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for assoc_list *)
+  let _ =
+    let y = ["a";"a";"b";"a"] in
+    let z = [1;7;7;1;5;2;7;7] in
+    let cmp x y = if x < y then (-1) else if x = y then 0 else 1 in
+    try
+      assert ([("a",3);("b",1)] = List.sort cmp (assoc_list y));
+      assert ([(1,2);(2,1);(5,1);(7,4)] = List.sort cmp (assoc_list z));
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  (* Testcases for ap *)
+  let _ =
+    let x = [5;6;7;3] in
+    let b = [3] in
+    let fs1 = [((+) 2) ; (( * ) 7)] in
+    try
+      assert  ([7;8;9;5;35;42;49;21] = ap fs1 x);
+      assert  ([5;21] = ap fs1 b);
+    with e -> (error_count := !error_count + 1; print_string ((Printexc.to_string e)^"\n")) in
+
+  Printf.printf ("%d out of 7 programming questions are incorrect.\n") (!error_count)
+
+let _ = main()
